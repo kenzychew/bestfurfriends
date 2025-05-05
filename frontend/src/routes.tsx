3ae67bba710a777a4426bada8df06601001f1041
kenzychew <kenzychew@gmail.com>
2025-05-05 14:11:54 +0800

    route creation

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter, RouteObject } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import OrderConfirmation from './pages/OrderConfirmation';
+import Account from './pages/Account';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'shop',
+        element: <Shop />,
+      },
+      {
+        path: 'products/:productId',
+        element: <ProductDetail />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'order-confirmation/:orderId',
+        element: <OrderConfirmation />,
+      },
+      {
+        path: 'account',
+        element: <Account />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+];
+
+export const router = createBrowserRouter(routes);
+
+export default routes; 
